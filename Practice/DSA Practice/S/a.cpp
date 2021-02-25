82e1ed2d790cfedeebe3b9804fbb17ea9f978647
ZainAhmad0 <zain80775@gmail.com>
2021-02-25 18:01:17 +0500

    ASG

--- Practice/DSA Practice/S/a.cpp
+++ Practice/DSA Practice/S/a.cpp
@@ -1,192 +1,8 @@
 #include <iostream>
+#include "sorting.h"
 using namespace std;
 
-void selectionSort(int *arr, const int size, int &compCount, int &moveCount)
-{
-    // finds the smallest element in arr and insert it at current position
-    int temp;
-    int min, m = 0;
-    for (int i = 0; i < size; i++)
-    {
-        min = i;
-        for (int j = i + 1; j < size; j++)
-        {
-            compCount++;
-            if (arr[j] < arr[min])
-            {
-                min = j;
-                m = 1;
-            }
-        }
-        if (m)
-        {
-            temp = arr[i];
-            arr[i] = arr[min];
-            arr[min] = temp;
-            moveCount += 3;
-            m = 0;
-        }
-    }
-}
-
-void merge(int *arr, int *temp, int lowerBound, int mid, int upperBound, int &compCount, int &moveCount)
-{
-    int i, j, k;
-    i = lowerBound;
-    k = lowerBound;
-    j = mid + 1;
-    while (i <= mid && j <= upperBound)
-    {
-        if (arr[i] <= arr[j])
-        {
-            temp[k] = arr[i];
-            i++;
-        }
-        else
-        {
-            temp[k] = arr[j];
-            j++;
-        }
-        compCount++;
-        moveCount++;
-        k++;
-    }
-    if (i > mid)
-    {
-        while (j <= upperBound)
-        {
-            temp[k] = arr[j];
-            k++;
-            j++;
-            moveCount++;
-        }
-    }
-    else
-    {
-        while (i <= mid)
-        {
-            temp[k] = arr[i];
-            k++;
-            i++;
-            moveCount++;
-        }
-    }
-    for (int p = lowerBound; p <= upperBound; p++)
-    {
-        arr[p] = temp[p];
-    }
-}
-
-void mergeSortHelper(int *arr, int *temparr, int lowerBound, int upperBound, int &compCount, int &moveCount)
-{
-    int mid;
-    if (lowerBound < upperBound)
-    {
-        mid = ((lowerBound + upperBound) / 2);
-        mergeSortHelper(arr, temparr, lowerBound, mid, compCount, moveCount);
-        mergeSortHelper(arr, temparr, mid + 1, upperBound, compCount, moveCount);
-        merge(arr, temparr, lowerBound, mid, upperBound, compCount, moveCount);
-    }
-}
-
-void mergeSort(int *arr, const int size, int &compCount, int &moveCount)
-{
-    int lowerBound = 0;
-    int upperBound = size;
-    int *tempArr = new int[size];
-    mergeSortHelper(arr, tempArr, lowerBound, upperBound, compCount, moveCount);
-}
-
-void swap(int &first, int &second)
-{
-    int temp = first;
-    first = second;
-    second = temp;
-}
-
-int partition(int *arr, int lowerBound, int upperBound, int &compCount, int &moveCount)
-{
-    int pivotPoint = arr[lowerBound];
-    int left = lowerBound;
-    int right = upperBound;
-    while (right > left)
-    {
-        while (arr[left] <= pivotPoint)
-        {
-            left++;
-            compCount++;
-        }
-        while (arr[right] > pivotPoint)
-        {
-            right--;
-            compCount++;
-        }
-        if (right > left)
-        {
-            swap(arr[left], arr[right]);
-            moveCount += 3;
-        }
-    }
-    arr[lowerBound] = arr[right];
-    arr[right] = pivotPoint;
-    moveCount += 2;
-    return right;
-}
-
-void quickSortHelper(int *array, int lowerBound, int upperBound, int &compCount, int &moveCount)
-{
-    if (upperBound > lowerBound)
-    {
-        int pivot = partition(array, lowerBound, upperBound, compCount, moveCount);
-        quickSortHelper(array, lowerBound, pivot - 1, compCount, moveCount);
-        quickSortHelper(array, pivot + 1, upperBound, compCount, moveCount);
-    }
-}
-
-void quickSort(int *arr, const int size, int &compCount, int &moveCount)
-{
-    int lowerBound, upperBound;
-    lowerBound = 0;
-    upperBound = size;
-    quickSortHelper(arr, lowerBound, upperBound, compCount, moveCount);
-}
-
-void radixSortHelper(int *arr, int size, int exp)
-{
-    int output[size];
-    int i, count[10] = {0};
-
-    for (i = 0; i < size; i++)
-        count[(arr[i] / exp) % 10]++;
-
-    for (i = 1; i < 10; i++)
-        count[i] += count[i - 1];
-
-    for (i = size - 1; i >= 0; i--)
-    {
-        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-        count[(arr[i] / exp) % 10]--;
-    }
-
-    for (i = 0; i < size; i++)
-        arr[i] = output[i];
-}
-
-void radixSort(int *arr, const int size)
-{
-    int mx;
-    mx = arr[0];
-    for (int i = 1; i < size; i++)
-    {
-        if (arr[i] > mx)
-            mx = arr[i];
-    }
-
-    for (int exp = 1; mx / exp > 0; exp *= 10)
-        radixSortHelper(arr, size, exp);
-}
-
-void disparray(int *arr, int size)
+void displayArray(const int *arr, const int size)
 {
     for (int i = 0; i < size; i++)
     {
@@ -194,34 +10,81 @@ void disparray(int *arr, int size)
     }
     cout << endl;
 }
+void createRandomArrays(int *&arr1, int *&arr2, int *&arr3, int *&arr4, const int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr1[i] = rand() % 100;
+        arr2[i] = arr1[i];
+        arr3[i] = arr1[i];
+        arr4[i] = arr1[i];
+    }
+}
+void createAscendingArrays(int *&arr1, int *&arr2, int *&arr3, int *&arr4, const int size)
+{
+    Sorting obj;
+    int compCount, moveCount;
+    compCount = moveCount = 0;
+    cout << "Arrays in ascending order" << endl;
+    obj.selectionSort(arr1, size, compCount, moveCount);
+    cout << "Selection sort" << endl;
+    displayArray(arr1, size);
+    cout << "Comparison = " << compCount << " & Data moves = " << moveCount << endl;
+    compCount = moveCount = 0;
+    obj.mergeSort(arr2, size, compCount, moveCount);
+    cout << "Merge sort" << endl;
+    displayArray(arr2, size);
+    cout << "Comparison = " << compCount << " & Data moves = " << moveCount << endl;
+    compCount = moveCount = 0;
+    obj.quickSort(arr3, size, compCount, moveCount);
+    cout << "Quick sort" << endl;
+    displayArray(arr3, size);
+    cout << "Comparison = " << compCount << " & Data moves = " << moveCount << endl;
+    cout << "Radix sort" << endl;
+    obj.radixSort(arr4, size);
+    displayArray(arr4, size);
+}
+void createDescendingArrays(int *&arr1, int *&arr2, int *&arr3, int *&arr4, const int size)
+{
+    Sorting obj;
+    cout << "Arrays in descending order" << endl;
+    int compCount, moveCount;
+    int temp1[size];
+    compCount = moveCount = 0;
+    obj.selectionSort(arr1, size, compCount, moveCount);
+    obj.mergeSort(arr2, size, compCount, moveCount);
+    obj.quickSort(arr3, size, compCount, moveCount);
+    obj.radixSort(arr4, size);
+    int j = 0;
+    for (int i = size - 1; i >= 0; i--)
+    {
+        temp1[j] = arr1[i];
+        arr2[j] = arr3[j] = arr4[j] = temp1[j];
+        j++;
+    }
+    arr1 = temp1;
+    cout << endl
+         << endl;
+    cout << "Selection sort" << endl;
+    displayArray(arr1, size);
+    cout << "Merge sort" << endl;
+    displayArray(arr2, size);
+    cout << "Quick sort" << endl;
+    displayArray(arr3, size);
+    cout << "Radix sort" << endl;
+    displayArray(arr4, size);
+}
 
 int main()
 {
-    int arr[] = {3, 7, 44, 6, -3, 65, 8, 9, 33, 2, 0};
-    int arr1[] = {3, 7, 44, 6, -3, 65, 8, 9, 33, 2, 0};
-    int arr2[] = {3, 7, 44, 6, -3, 65, 8, 9, 33, 2, 0};
-    int compCount = 0;
-    int moveCount = 0;
-    const int size = 11;
-    cout << "Original Array:" << endl;
-    disparray(arr, size);
-    cout << "Array after selection sort:" << endl;
-    selectionSort(arr, size, compCount, moveCount);
-    disparray(arr, size);
-    cout << "Comparisons = " << compCount << " &  Moves = " << moveCount << endl;
-    cout << endl;
-    cout << "Array after Merge sort:" << endl;
-    compCount = 0;
-    moveCount = 0;
-    mergeSort(arr1, size, compCount, moveCount);
-    disparray(arr1, size);
-    cout << "Comparisons = " << compCount << " &  Moves = " << moveCount << endl;
-    cout << endl;
-    compCount = 0;
-    moveCount = 0;
-    quickSort(arr2, size, compCount, moveCount);
-    disparray(arr2, size);
-    cout << "Comparisons = " << compCount << " &  Moves = " << moveCount << endl;
-    
+
+    const int size = 10;
+    int *arr1 = new int[size];
+    int *arr2 = new int[size];
+    int *arr3 = new int[size];
+    int *arr4 = new int[size];
+    createRandomArrays(arr1, arr2, arr3, arr4, size);
+    createAscendingArrays(arr1, arr2, arr3, arr4, size);
+    createDescendingArrays(arr1, arr2, arr3, arr4, size);
     return 0;
 }
--- Practice/DSA Practice/S/sorting.h
+++ Practice/DSA Practice/S/sorting.h
@@ -1,3 +1,192 @@
 #include <iostream>
 using namespace std;
 
+class Sorting
+{
+private:
+    void radixSortHelper(int *arr, int size, int exp)
+    {
+        int output[size];
+        int i, count[10] = {0};
+
+        for (i = 0; i < size; i++)
+            count[(arr[i] / exp) % 10]++;
+
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (i = size - 1; i >= 0; i--)
+        {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (i = 0; i < size; i++)
+            arr[i] = output[i];
+    }
+
+    void merge(int *arr, int *temp, int lowerBound, int mid, int upperBound, int &compCount, int &moveCount)
+    {
+        int i, j, k;
+        i = lowerBound;
+        k = lowerBound;
+        j = mid + 1;
+        while (i <= mid && j <= upperBound)
+        {
+            if (arr[i] <= arr[j])
+            {
+                temp[k] = arr[i];
+                i++;
+            }
+            else
+            {
+                temp[k] = arr[j];
+                j++;
+            }
+            compCount++;
+            moveCount++;
+            k++;
+        }
+        if (i > mid)
+        {
+            while (j <= upperBound)
+            {
+                temp[k] = arr[j];
+                k++;
+                j++;
+                moveCount++;
+            }
+        }
+        else
+        {
+            while (i <= mid)
+            {
+                temp[k] = arr[i];
+                k++;
+                i++;
+                moveCount++;
+            }
+        }
+        for (int p = lowerBound; p <= upperBound; p++)
+        {
+            arr[p] = temp[p];
+        }
+    }
+
+    void mergeSortHelper(int *arr, int *temparr, int lowerBound, int upperBound, int &compCount, int &moveCount)
+    {
+        int mid;
+        if (lowerBound < upperBound)
+        {
+            mid = ((lowerBound + upperBound) / 2);
+            mergeSortHelper(arr, temparr, lowerBound, mid, compCount, moveCount);
+            mergeSortHelper(arr, temparr, mid + 1, upperBound, compCount, moveCount);
+            merge(arr, temparr, lowerBound, mid, upperBound, compCount, moveCount);
+        }
+    }
+
+    void swap(int &first, int &second)
+    {
+        int temp = first;
+        first = second;
+        second = temp;
+    }
+
+    int partition(int *arr, int lowerBound, int upperBound, int &compCount, int &moveCount)
+    {
+        int pivotPoint = arr[lowerBound];
+        int left = lowerBound;
+        int right = upperBound;
+        while (right > left)
+        {
+            while (arr[left] <= pivotPoint)
+            {
+                left++;
+                compCount++;
+            }
+            while (arr[right] > pivotPoint)
+            {
+                right--;
+                compCount++;
+            }
+            if (right > left)
+            {
+                swap(arr[left], arr[right]);
+                moveCount += 3;
+            }
+        }
+        arr[lowerBound] = arr[right];
+        arr[right] = pivotPoint;
+        moveCount += 2;
+        return right;
+    }
+
+    void quickSortHelper(int *array, int lowerBound, int upperBound, int &compCount, int &moveCount)
+    {
+        if (upperBound > lowerBound)
+        {
+            int pivot = partition(array, lowerBound, upperBound, compCount, moveCount);
+            quickSortHelper(array, lowerBound, pivot - 1, compCount, moveCount);
+            quickSortHelper(array, pivot + 1, upperBound, compCount, moveCount);
+        }
+    }
+
+public:
+    void selectionSort(int *arr, const int size, int &compCount, int &moveCount)
+    {
+        // finds the smallest element in arr and insert it at current position
+        int temp;
+        int min, m = 0;
+        for (int i = 0; i < size; i++)
+        {
+            min = i;
+            for (int j = i + 1; j < size; j++)
+            {
+                compCount++;
+                if (arr[j] < arr[min])
+                {
+                    min = j;
+                    m = 1;
+                }
+            }
+            if (m)
+            {
+                temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+                moveCount += 3;
+                m = 0;
+            }
+        }
+    }
+
+    void mergeSort(int *arr, const int size, int &compCount, int &moveCount)
+    {
+        int lowerBound = 0;
+        int upperBound = size;
+        int *tempArr = new int[size];
+        mergeSortHelper(arr, tempArr, lowerBound, upperBound, compCount, moveCount);
+    }
+
+    void quickSort(int *arr, const int size, int &compCount, int &moveCount)
+    {
+        int lowerBound, upperBound;
+        lowerBound = 0;
+        upperBound = size;
+        quickSortHelper(arr, lowerBound, upperBound, compCount, moveCount);
+    }
+
+    void radixSort(int *arr, const int size)
+    {
+        int mx;
+        mx = arr[0];
+        for (int i = 1; i < size; i++)
+        {
+            if (arr[i] > mx)
+                mx = arr[i];
+        }
+
+        for (int exp = 1; mx / exp > 0; exp *= 10)
+            radixSortHelper(arr, size, exp);
+    }
+};
